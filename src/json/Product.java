fdf83b2f51ead2409765b5949f379aedd178bddd
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2016-03-29 11:49:37 +0400

    ....

--- src/json/Product.java
+++ src/json/Product.java
@@ -1,10 +1,10 @@
 package json;
 
-public class Product {
-        public int id;
-        public String description;
-        public double price;
-    public Product(int id, String description, double price){
+class Product {
+        int id;
+        String description;
+        double price;
+    Product(int id, String description, double price){
         this.id=id;
         this.description=description;
         this.price=price;
